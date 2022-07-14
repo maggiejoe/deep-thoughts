9c2271912110d8966cb99a707d14bc2f7bb898d2
maggiejoe <maggiejoseph94@gmail.com>
2022-07-14 15:37:07 -0400

    committing for assistance with BCS on console error in browser

--- server/server.js
+++ server/server.js
@@ -1,9 +1,9 @@
 const express = require('express');
-const { ApolloServer } = require('apollo-server-express');
-const { authMiddleware } = require('./utils/auth');
+const {ApolloServer} = require('apollo-server-express');
 const path = require('path');
 
-const { typeDefs, resolvers } = require('./schemas');
+const {typeDefs, resolvers} = require('./schemas');
+const {authMiddleware} = require('./utils/auth');
 const db = require('./config/connection');
 
 const PORT = process.env.PORT || 3002;
@@ -13,7 +13,7 @@ const startServer = async () => {
   const server = new ApolloServer({
     typeDefs,
     resolvers,
-    context: authMiddleware
+    context: authMiddleware,
   });
   await server.start();
   server.applyMiddleware({ app });
@@ -32,7 +32,7 @@ if (process.env.NODE_ENV === 'production') {
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
-})
+});
 
 db.once('open', () => {
   app.listen(PORT, () => {
